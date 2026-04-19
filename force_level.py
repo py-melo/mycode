0c5129de280bb807a62eea449a89be119d35fe7c
CeeLo <chiikiitomelo@gmail.com>
2026-04-19 22:00:31 +0000

    first backup of scripts

---
+++ force_level.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Script that collects each Star Wars character's force level thats input
+
+#This prints and accepts user input in one line of code.
+
+chosen_character = input("Alright nerd what character are you curious about today?" ) 
+force_level = input("How strong are ya nerd?" )
+
+#This prints the message using the input , remember to use print(f) for this part
+
+print(f"Okay nerd, so {chosen_character} has a power determination of {force_level} !!")
+
+
