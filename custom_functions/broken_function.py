989b7a9ba1b0187f574b94092aa17ba1226a3e0d
CeeLo <chiikiitomelo@gmail.com>
2026-04-27 02:12:41 +0000

    backing up work

---
+++ custom_functions/broken_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Broken Function"""
+
+def report_ghost_sighting(ghost_name, ):
+    print(f"{ghost_name} has been sighted! Who you gonna call?")
+
+report_ghost_sighting("Slimer")
